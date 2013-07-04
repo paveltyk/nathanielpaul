2f45ecf56c16fecb1eaeba9cac51821d25ca3ff3
Extazystas <z1m@tut.by>
2013-07-05 00:22:40 +0300

    Add request specs

--- spec/controllers/root_controller_spec.rb
+++ spec/controllers/root_controller_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 
 describe RootController do
   describe '#landing' do
-    render_views
-
     before { get :landing }
 
     it { should respond_with(:success) }
@@ -12,12 +10,18 @@ describe RootController do
   end
 
   describe '#about' do
-    render_views
-
     before { get :about }
 
     it { should respond_with(:success) }
     it { should render_template(:about) }
+    it { should_not set_the_flash }
+  end
+
+   describe '#contact' do
+    before { get :contact }
+
+    it { should respond_with(:success) }
+    it { should render_template(:contact) }
     it { should_not set_the_flash }
   end
 end
