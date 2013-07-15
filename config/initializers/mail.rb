ae45bbc6c099a0c8a6bdf374b9e70ca21e83c5b7
Stanislav Knyazev <knyazev@ziex.by>
2013-07-15 19:35:15 +0300

    Add mail.rb initializer

---
+++ config/initializers/mail.rb
@@ -0,0 +1,17 @@
+ActionMailer::Base.register_interceptor(SendGrid::MailInterceptor)
+
+if ENV['SENDGRID_USERNAME'] && ENV['SENDGRID_PASSWORD']
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+  ActionMailer::Base.delivery_method = :smtp
+end
+
+SendGrid.configure do |config|
+  config.dummy_recipient = ENV['CONTACT_EMAIL']
+end
