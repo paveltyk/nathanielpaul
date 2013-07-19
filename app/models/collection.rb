6a7b81ac13b9b4ed14016ed75e6559fd16687de9
Stanislav Knyazev <knyazev@ziex.by>
2013-07-19 13:00:37 +0300

    Add deactivation callback to collection model

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,3 +1,11 @@
 class Collection < ActiveRecord::Base
   validates :name, :presence => true
+
+  after_save :deactivate_active_collections, :if => 'active && active_changed?'
+
+  private
+
+  def deactivate_active_collections
+    Collection.where(:active => true).where('id != ?', id).update_all(:active => false)
+  end
 end
