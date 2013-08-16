e18c4f36a74005cba7b27b7348abca85ccc2a8b2
PavelT <paveltyk@gmail.com>
2013-08-16 20:46:35 +0300

    Code formatting

--- spec/requests/collection_item_spec.rb
+++ spec/requests/collection_item_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe 'Collection item page' do
-	let!(:collection) { create :collection }
-  	let!(:item) { create :collection_item, :collection => collection }
+  let!(:collection) { create :collection }
+  let!(:item) { create :collection_item, :collection => collection }
 
-  	context 'visit /collection/items/:id' do
-    	before { visit collection_item_path(item) }
-    	it { page.status_code.should eq 200 }
- 	end
+  context 'visit /collection/items/:id' do
+    before { visit collection_item_path(item) }
+    it { page.status_code.should eq 200 }
+  end
 end
